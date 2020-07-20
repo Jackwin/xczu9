715017adef161f41448d3d843e7fcc12358f945f
jackwin <rcwcj@126.com>
2020-07-20 23:17:37 +0800

    Update to 2018.3 and compilation PASSED

--- script/mpsoc.tcl
+++ script/mpsoc.tcl
@@ -20,7 +20,7 @@ set script_folder [_tcl::get_script_folder]
 ################################################################
 # Check if script is running in correct Vivado version.
 ################################################################
-set scripts_vivado_version 2017.4
+set scripts_vivado_version 2018.3
 set current_vivado_version [version -short]
 
 if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
@@ -122,7 +122,7 @@ set bCheckIPs 1
 if { $bCheckIPs == 1 } {
    set list_check_ips "\ 
 xilinx.com:ip:gmii_to_rgmii:4.0\
-xilinx.com:ip:zynq_ultra_ps_e:3.1\
+xilinx.com:ip:zynq_ultra_ps_e:3.2\
 "
 
    set list_ips_missing ""
@@ -211,7 +211,7 @@ proc create_root_design { parentCell } {
  ] $gmii_to_rgmii_0
 
   # Create instance: zynq_ultra_ps_e_0, and set properties
-  set zynq_ultra_ps_e_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.1 zynq_ultra_ps_e_0 ]
+  set zynq_ultra_ps_e_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.2 zynq_ultra_ps_e_0 ]
   set_property -dict [ list \
    CONFIG.CAN0_BOARD_INTERFACE {custom} \
    CONFIG.CAN1_BOARD_INTERFACE {custom} \
