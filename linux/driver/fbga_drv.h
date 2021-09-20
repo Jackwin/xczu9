3256633a984059a3ed7230e4616ba741531aba95
qinlei <delphiqin@foxmail.com>
2021-09-20 12:06:26 +0800

    Add: test app; Update: drvier

--- linux/driver/fbga_drv.h
+++ linux/driver/fbga_drv.h
@@ -14,12 +14,34 @@
 #define TIMEOUT_MS_DMATX    100
 
 #define DMA_REGOFF_RESET    0x0
+
 #define DMA_REGOFF_TXEN     0x0100
+
 #define DMA_REGOFF_TXADDR   0x0108
-#define DMA_REGOFF_TXLEN    0x0008
-#define DMA_REGOFF_TXTB     0x0118  //tail len and body len
-#define DMA_REGOFF_TXMOD    0x0120  //transe mode and body number
+
+#define DMA_REGOFF_TXLEN    0x0110
+
+#define DMA_REGOFF_TXPACK   0x0118  //trans package config: tail len and body len
+#define DMA_TXPACK_MODE_SHIFT   60  //mode shift 
+#define DMA_TXPACK_TAILLEN_SHIFT    32  //tail len shift
+#define DMA_TXPACK_BODYNUM_SHIFT    15  //body num shift
+#define DMA_TXPACK_DATALEN_SHIFT    0  //valid data len shift
+
+#define DMA_REGOFF_TXSR   0x0120  //trans status
+#define DMA_TXSR_FIFOEMP_MASK  BIT(9)
+#define DMA_TXSR_FIFOFULL_MASK  BIT(8)
+#define DMA_TXSR_SM_MASK    GENMASK(7,4)    //status machine
+#define DMA_TXSR_MODE_MASK  GENMASK(3,0)    //
+
 #define DMA_REGOFF_RXADDR   0x0208
+
+#define DMA_REGOFF_RXCTRL   0x0210
+#define DMA_RXCTRL_FIFOEN_MASK  BIT(0)
+
+#define DMA_REGOFF_RXSR 0x0210
+#define DMA_RXSR_FIFOEMP_MASK  BIT(5)
+#define DMA_RXSR_FIFOFULL_MASK  BIT(4)
+#define DMA_RXSR_MODE_MASK  GENMASK(3,0)
 
 #define DMA_REGOFF_INTSR    0x0110
 #define DMA_INTSR_TYPE_MASK GENMASK(63,60)
